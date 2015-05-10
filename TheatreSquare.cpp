692e7681f1ba155c83482dd39ce0fd213b2f459a
Aditi Dixit <aditi96dixit@gmail.com>
2015-05-11 01:50:16 +0530

    Updated solution

--- TheatreSquare.cpp
+++ TheatreSquare.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cmath>
 
 
 using namespace std;
@@ -6,33 +7,15 @@ using namespace std;
 int main ()
 
 {
-      int flagstone_side, theatre_length, theatre_breadth, theatre_area, flagstone_area, n;
+      float flagstone_side, theatre_length, theatre_breadth, x, y, n;
 
 cin >> theatre_length >> theatre_breadth >> flagstone_side;
 
- theatre_area = theatre_length * theatre_breadth;
- flagstone_area = flagstone_side * flagstone_side;
- if (theatre_area > flagstone_area) {
- 
- n = theatre_area / flagstone_area; 
-  if (theatre_area % flagstone_area != 0) {
-n=n+1 ;
+x = ceil(theatre_length/flagstone_side);
 
-}
+y = ceil(theatre_breadth/flagstone_side);
 
-if (n % 2 != 0) { 
-
-n = n+1;
-
-}
-
-cout << n;
-
-} 
-
-else cout << "1";
-
-
+cout << x * y;
 
 return 0;
 
