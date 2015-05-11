c06994d76761779f27c25cf0c6ab540bc3148b6e
Aditi Dixit <aditi96dixit@gmail.com>
2015-05-11 23:42:59 +0530

    Added Temperature.cpp

---
+++ Temperature.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+float kChirps_per_min , temperature ;
+
+float temp(float kChirps_per_min, float temperature);
+
+cout << "Enter number of chirps per minute " ;
+
+cin >> kChirps_per_min ; 
+
+cout << "The temperature is: " << temp(kChirps_per_min, temperature) ;
+
+
+
+}
+
+float temp(float kChirps_per_min, float temperature)  {
+
+kChirps_per_min = kChirps_per_min + 40 ;
+
+temperature = kChirps_per_min / 4 ;
+
+return (temperature); 
+
+}
+
+
+
+
